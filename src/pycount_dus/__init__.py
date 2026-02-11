8bba0940ce58d7350434c46b7568ceb01a31d75a
= <=>
2026-02-11 19:42:35 -0300

    feat: add example data and datasets module

--- src/pycount_dus/__init__.py
+++ src/pycount_dus/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
-__version__ = version("pycount_dus")
+__version__ = version(__name__)
+
+# populate package namespace
+from pycount_dus.pycount_dus import count_words
+from pycount_dus.plotting import plot_words
